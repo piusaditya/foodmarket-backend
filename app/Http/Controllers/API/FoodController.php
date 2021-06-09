632d3ee521dea7bfcef43c156e474228f54869db
piusaditya <piusadityanugraha@gmail.com>
2021-06-09 17:16:39 +0700

    API food

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\Food;
 use Illuminate\Http\Request;
 
 class FoodController extends Controller
@@ -12,6 +14,7 @@ class FoodController extends Controller
     {
         $id = $request->input('id');
         $limit = $request->input('limit', 6);
+        $name = $request->input('name');
         $types = $request->input('types');
 
         $price_from = $request->input('price_from');
@@ -19,5 +22,53 @@ class FoodController extends Controller
 
         $rate_from = $request->input('rate_from');
         $rate_to = $request->input('rate_to');
+
+        if ($id) {
+            $food = Food::find($id);
+
+            if ($food) {
+                return ResponseFormatter::success(
+                    $food,
+                    'Data produk berhasil diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data produk tidak ada',
+                    404
+                );
+            }
+        }
+
+        $food = Food::query();
+
+        if ($name) {
+            $food->where('name', 'like', '%' . $name . '%');
+        }
+
+        if ($types) {
+            $food->where('types', 'like', '%' . $types . '%');
+        }
+
+        if ($price_from) {
+            $food->where('price', '>=', $price_from);
+        }
+
+        if ($price_to) {
+            $food->where('price', '<=', $price_to);
+        }
+
+        if ($rate_from) {
+            $food->where('rate', '>=', $rate_from);
+        }
+
+        if ($rate_to) {
+            $food->where('rate', '<=', $rate_to);
+        }
+
+        return ResponseFormatter::success(
+            $food->paginate($limit),
+            'Data list produk berhasil diambil'
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
+
+
+Route::post('food', [FoodController::class, 'all']);
